744009fcc113b4af73c7ab692ac128b57d48f6bd
TurtlePult <ivan_ladiygin@mail.ru>
2023-06-06 19:17:26 +0500

    perhabs a working programm has been created

---
+++ hw001/t_1.java
@@ -0,0 +1,46 @@
+package hw001;
+
+public class t_1 {
+    public class Cat { private String name; private int age; private String ownerName;
+
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public int getAge() {
+            return age;
+        }
+        
+        public void setAge(int age) {
+            this.age = age;
+        }
+        
+        public String getOwnerName() {
+            return ownerName;
+        }
+        
+        public void setOwnerName(String ownerName) {
+            this.ownerName = ownerName;
+        }
+        
+        public void greet() {
+            System.out.println("Мяу! Меня зовут " + name + ". Мне " + age + " года(лет). Мой владелец - " + ownerName + ".");
+        }
+        }
+        
+        public class Owner { private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+        }
+    
+}
