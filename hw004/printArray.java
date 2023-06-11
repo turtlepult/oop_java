524d974ed7b8c31347aff99f565bc78697987d64
TurtlePult <ivan_ladiygin@mail.ru>
2023-06-11 09:19:50 +0500

    perhabs a working programm has been created

---
+++ hw004/printArray.java
@@ -0,0 +1,10 @@
+package hw004;
+
+public class printArray {
+    public static  void printArray(T[] arr) {
+    for (T element : arr) {
+        System.out.print(element + " ");
+    }
+        System.out.println();
+}
+}
---
+++ hw004/printElement.java
@@ -0,0 +1,6 @@
+package hw004;
+
+public class printElement {
+    public static <T> void printElement(T element) {
+    }
+}
